551a072f7bf22b958086967644cb8257b8ce17ae
Michael White <github@mypalmike.com>
2020-01-13 22:01:28 -0800

    First commits, Lesson01 and Lesson02

---
+++ Lesson01/src/main.zig
@@ -0,0 +1,82 @@
+const std = @import("std");
+const warn = std.debug.warn;
+const panic = std.debug.panic;
+const c = @import("c.zig");
+
+const width: i32 = 1024;
+const height: i32 = 768;
+
+var window: *c.GLFWwindow = undefined;
+
+extern fn errorCallback(err: c_int, description: [*c]const u8) void {
+    panic("Error: {}\n", description);
+}
+
+extern fn keyCallback(win: ?*c.GLFWwindow, key: c_int, scancode: c_int, action: c_int, mods: c_int) void {
+    if (action != c.GLFW_PRESS) return;
+
+    switch (key) {
+        c.GLFW_KEY_ESCAPE => c.glfwSetWindowShouldClose(win, c.GL_TRUE),
+        else => {},
+    }
+}
+
+fn perspectiveGL(fovY: f64, aspect: f64, zNear: f64, zFar: f64) void {
+    const fH = std.math.tan(fovY / 360 * std.math.pi) * zNear;
+    const fW = fH * aspect;
+    c.glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
+
+fn init_gl() void {
+    c.glMatrixMode(c.GL_PROJECTION);                    // Select The Projection Matrix
+    c.glLoadIdentity();
+
+    var aspect_ratio: f32 = f32(height) / f32(width);
+    perspectiveGL(45.0, (1.0 / aspect_ratio), 0.1, 100.0);
+
+    c.glMatrixMode(c.GL_MODELVIEW);
+    c.glShadeModel(c.GL_SMOOTH);                        // Enables Smooth Shading
+    c.glClearColor(0.0, 0.0, 0.0, 0.0);                 // Black Background
+    c.glClearDepth(1.0);                                // Depth Buffer Setup
+    c.glEnable(c.GL_DEPTH_TEST);                        // Enables Depth Testing
+    c.glDepthFunc(c.GL_LEQUAL);                         // The Type Of Depth Test To Do
+    c.glHint(c.GL_PERSPECTIVE_CORRECTION_HINT, c.GL_NICEST);  // Really Nice Perspective Calculations
+}
+
+fn init() bool {
+    _ = c.glfwSetErrorCallback(errorCallback);
+
+    if (c.glfwInit() == c.GL_FALSE) {
+        warn("Failed to initialize GLFW\n");
+        return false;
+    }
+
+    c.glfwWindowHint(c.GLFW_SAMPLES, 4);                // 4x antialiasing
+
+    window = c.glfwCreateWindow(width, height, c"Lesson 1", null, null) orelse {
+        panic("unable to create window\n");
+    };
+
+    _ = c.glfwSetKeyCallback(window, keyCallback);
+    c.glfwMakeContextCurrent(window);
+    c.glfwSwapInterval(1);
+    
+    init_gl();
+    return true;
+}
+
+pub fn main() u8 {
+    if (!init()) {
+        return 1;
+    }
+
+    while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
+        // Draw nothing, see you in tutorial 2 !
+        // Swap buffers
+        c.glfwSwapBuffers(window);
+
+        c.glfwPollEvents();
+    }
+
+    return 0;
+}
---
+++ Lesson02/build.zig
@@ -0,0 +1,20 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("Lesson02", "src/main.zig");
+    exe.setBuildMode(mode);
+
+    exe.addFrameworkDir("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks");
+    exe.addIncludeDir("/usr/local/include");
+    exe.linkSystemLibrary("glfw");
+    exe.linkFramework("OpenGL");
+
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
